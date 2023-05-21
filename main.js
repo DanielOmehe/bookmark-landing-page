0334da5c2130df4a723f3b1e9b547e70ef9f66d7
DaniOmehe <danielomehe66@gmail.com>
2023-05-21 23:41:05 +0100

    write logic to add functionality

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const closeBtn = document.querySelector('.close-menu');
 const menu = document.querySelector('.menu');
 const navbar = document.querySelector('.navigation');
 let isOpen = false;
+const tabs = document.querySelectorAll('.tab-item');
+const tabContents = document.querySelectorAll('.features-tab');
+const arrows = document.querySelectorAll('.arrow');
+const questions = document.querySelectorAll('.question');
 
 menuBtn.addEventListener('click', ()=>{
     if(!isOpen) {
@@ -19,3 +23,36 @@ closeBtn.addEventListener('click', ()=>{
         navbar.classList.remove('open');
     }
 })
+
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', ()=>{
+        tabs.forEach(tabs => tabs.classList.remove('active'))
+        tab.classList.add('active');
+
+        tabContents.forEach(content => content.classList.remove('active'))
+        tabContents[index].classList.add('active');
+    })
+});
+
+arrows.forEach((arrow, index) => {
+    arrow.addEventListener('click', ()=>{
+        arrows.forEach(arrows => arrows.classList.remove('active'))
+        if(!isOpen) {
+            arrow.classList.add('active');
+        }else{
+            arrow.classList.remove('active');
+        }
+
+        questions.forEach(question => question.classList.remove('active'));
+        if(!isOpen){
+            questions[index].classList.add('active');
+            questions[index].firstChild.nextSibling.style.color = 'red';
+            isOpen = true;
+        }else{
+            isOpen = false;
+            questions[index].classList.remove('active');
+            questions[index].firstChild.nextSibling.style.color = 'black';
+        }
+    })
+});
